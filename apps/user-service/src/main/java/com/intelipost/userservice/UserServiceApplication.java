d876c1f0d82e5097ea018bf0b4a6d3edeb061cc6
rafael.bertoli <Rafael_2@PC-ADM>
2018-06-23 14:50:20 -0300

    Adicionado dependencia e configuração para fazer cache com redis. Adicionado dependencia para iniciar um redis embedded.

--- apps/user-service/src/main/java/com/intelipost/userservice/UserServiceApplication.java
+++ apps/user-service/src/main/java/com/intelipost/userservice/UserServiceApplication.java
@@ -1,12 +1,32 @@
 package com.intelipost.userservice;
 
+import com.github.caryyu.spring.embedded.redisserver.RedisServerConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 
+@EnableCaching
 @SpringBootApplication
 public class UserServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    /**
+     * Bean para inicialização do servidor redis embedded.
+     * <p>
+     * Os seguintes atributos podem ser custumizados:
+     * <p>
+     * spring.redis.embedded= true/false (Habilita serviço redis embedded ou
+     * não)
+     * spring.redis.port=22001 (Porta que sera inicializado o serviço)
+     * </p> 
+     * @return configuração do servidor redis embedded
+     */
+    @Bean
+    public RedisServerConfiguration redisServerConfiguration() {
+        return new RedisServerConfiguration();
     }
 }
