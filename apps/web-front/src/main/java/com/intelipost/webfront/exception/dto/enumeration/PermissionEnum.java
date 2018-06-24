6ab565c1cc5ac82eb45d3848eaac2b945701ef12
rafael.bertoli <Rafael_2@PC-ADM>
2018-06-24 00:03:24 -0300

    Enum de permissão

---
+++ apps/web-front/src/main/java/com/intelipost/webfront/exception/dto/enumeration/PermissionEnum.java
@@ -0,0 +1,35 @@
+package com.intelipost.webfront.exception.dto.enumeration;
+
+/**
+ * Enum de permissões
+ * @author Rafael
+ */
+public enum PermissionEnum {
+    
+    USER("Perfil","/user"),
+    PRODUCT("Produtos","#"),
+    NFE("NF-e","#"),
+    PURCHASE_ORDER("Ordem de Compra","#"),
+    SALE("Venda","#");
+    
+    private String view;
+    private String viewPath;
+
+    private PermissionEnum(String view, String viewPath) {
+        this.view = view;
+        this.viewPath = viewPath;
+    }
+
+    public Integer getOrder() {
+        return ordinal();
+    }
+    
+    public String getView() {
+        return view;
+    }
+
+    public String getViewPath() {
+        return viewPath;
+    }
+    
+}
