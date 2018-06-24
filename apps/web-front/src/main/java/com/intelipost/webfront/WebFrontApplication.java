a88924d04b9115b5aecb1f847fbd09ca77805b7d
rafael.bertoli <Rafael_2@PC-ADM>
2018-06-24 00:01:53 -0300

    Estrutura do módulo web-front

---
+++ apps/web-front/src/main/java/com/intelipost/webfront/WebFrontApplication.java
@@ -0,0 +1,12 @@
+package com.intelipost.webfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebFrontApplication.class, args);
+    }
+}
